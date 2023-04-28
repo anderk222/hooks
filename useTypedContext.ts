e4305ac7872991bb97fc04c1230c77cc656881bd
anderk222 <117754488+anderk222@users.noreply.github.com>
2023-04-28 17:21:21 -0500

    Create useTypedContext.ts

---
+++ useTypedContext.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+
+export function useTypedContext<T>(context : React.Context<T>) {
+
+  const ctx = useContext(context);
+
+  if (!ctx) throw new Error('you must set your component into <AuthProvider>');
+
+  return ctx;
+}
